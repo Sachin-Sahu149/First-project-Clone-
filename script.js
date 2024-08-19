fdae3d65e8fe85f78e857d601079fc2de570db9c
Sachin-Sahu149 <sachinsahu3222@gmail.com>
2024-08-19 11:08:39 +0530

    First project clone

---
+++ script.js
@@ -0,0 +1,28 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
+let featuredSection = document.querySelectorAll(".div-box");
+let fixedImage = document.querySelector(".fixed-image");
+// featuredSection.forEach(function(e){
+//     fixedImage.style.display = "none";
+    
+// });
+featuredSection.forEach(function(e){
+    e.addEventListener("mouseover",()=>{
+        let image = e.getAttribute("image-url");
+        console.log(image);
+        fixedImage.style.backgroundImage = `url(${image})`;
+        fixedImage.style.backgroundPosition = "center";
+        fixedImage.style.backgroundRepeat = "no-repeat";
+        fixedImage.style.backgroundSize = "cover";
+        fixedImage.style.display = "block";
+    })
+});
+featuredSection.forEach(function(e){
+    e.addEventListener("mouseleave",()=>{
+        fixedImage.style.display = "none";
+    })
+});
